e2dc7ae857c42d7b6f280f0d92da39e9791d97a5
Ishpreet Singh <ishpreet232sin@gmail.com>
2024-11-09 16:17:15 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("but").onclick=function()
-{
-let user_name=document.getElementById("user").value;
-let pass_=document.getElementById("pass").value;
-alert(`Welcome ${user_name}`)
-window.open('home.html');
-}
